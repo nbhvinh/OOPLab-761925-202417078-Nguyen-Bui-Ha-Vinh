8a262d9551a02dea0eb9995318cdc02a527d1520
nbhvinh <nguyenbuihavinh@gmail.com>
2026-03-27 13:51:04 +0700

    HelloNameDialog nop

---
+++ HelloNameDialog.java
@@ -0,0 +1,13 @@
+package Lab01;
+
+import javax.swing.JOptionPane;
+
+public class HelloNameDialog {
+    public static void main(String[] args) {
+        String res;
+        res = JOptionPane.showInputDialog("Pls enter ur name");
+        JOptionPane.showMessageDialog(null, "Hi " + res + "!");
+        System.exit(0);
+    }
+    
+}
