718ae3af4a8096ea9b138121d7830ca506e9753b
nbhvinh <nguyenbuihavinh@gmail.com>
2026-03-27 23:20:25 +0700

    Upload lai toan bo bai lam Lab01

---
+++ ShowTwoNumbers.java
@@ -0,0 +1,20 @@
+
+
+import javax.swing.JOptionPane;
+
+public class ShowTwoNumbers {
+    public static void main(String[] args) {
+        String num1, num2;
+        String Noti = "You've just entered: ";
+        
+        num1 = JOptionPane.showInputDialog(null, "Please input the 1st number: ", "Input the 1st number",JOptionPane.INFORMATION_MESSAGE);
+        Noti += num1 + " and ";
+        
+        num2 = JOptionPane.showInputDialog(null, "Please input the 2nd number: ", "Input the 2nd number", JOptionPane.INFORMATION_MESSAGE);
+        Noti += num2;
+
+        JOptionPane.showConfirmDialog(null, Noti, "Show 2 numbers", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+    
+}
