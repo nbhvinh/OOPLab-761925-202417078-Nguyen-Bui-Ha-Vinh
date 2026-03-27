a1c3ea0fbc9898ace6800a203026fc85651c1565
nbhvinh <nguyenbuihavinh@gmail.com>
2026-03-27 13:47:03 +0700

    FirstDialog nop

---
+++ FirstDialog.java
@@ -0,0 +1,10 @@
+package Lab01;
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello");
+        System.exit(0);
+    }
+}
