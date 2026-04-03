dd4e5ce791410c65834f46a01ff3b12124811338
nbhvinh <nguyenbuihavinh@gmail.com>
2026-04-03 21:47:48 +0700

    Topic: Overloading tested with duplicate items and verified total cost

--- Lab02/SuocCode/Aims.java
+++ Lab02/SuocCode/Aims.java
@@ -1,16 +1,16 @@
 public class Aims {
     public static void main(String[] args) {
         Cart cart = new Cart();
-        
+
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", 19.95f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Sci-Fi", 24.95f);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 
-        cart.addDigitalVideoDisc(dvd1, dvd2, dvd3);
+        DigitalVideoDisc[] list = {dvd1, dvd2};
+        cart.addDigitalVideoDisc(list);
+
+        cart.addDigitalVideoDisc(dvd2, dvd3);
 
         cart.print();
-
-        cart.search(1);
-        cart.search("Star");
     }
 }
--- Lab02/SuocCode/Cart.java
+++ Lab02/SuocCode/Cart.java
@@ -15,15 +15,15 @@ public class Cart {
     }
 
 
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
+        for (DigitalVideoDisc disc : dvdList) {
+            addDigitalVideoDisc(disc); 
+        }
+    }
+
     public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
         addDigitalVideoDisc(dvd1);
         addDigitalVideoDisc(dvd2);
-    }
-
-    public void addDigitalVideoDisc(DigitalVideoDisc... dvds) {
-        for (DigitalVideoDisc disc : dvds) {
-            addDigitalVideoDisc(disc);
-        }
     }
 
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
